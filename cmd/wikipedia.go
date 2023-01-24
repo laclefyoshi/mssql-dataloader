b95894ba636b002670dd6405f438e98244a4cf97
Ubuntu <yosaeki@yosaeki-linux.5uv3nrwndwlejmyzx5wndi5xsa.xx.internal.cloudapp.net>
2023-01-24 06:54:19 +0000

    edit help messages

--- cmd/wikipedia.go
+++ cmd/wikipedia.go
@@ -24,13 +24,8 @@ import (
 // wikipediaCmd represents the wikipedia command
 var wikipediaCmd = &cobra.Command{
 	Use:   "wikipedia",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "load wikipedia page data",
+	Long: `.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("wikipedia called")
 		readDataset()
